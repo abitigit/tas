e1d1c4e517de4422e0dfb9239230fee10a476143
Yohannes Mesay <johnnymesay@gmail.com>
2025-08-16 14:25:02 +0300

    ff

--- src/task3.sql
+++ src/task3.sql
@@ -1,10 +1,10 @@
 SELECT 
     u.Name as name,
     CONCAT(
-        LOWER(SUBSTRING(u.username, 1, 2)),  -- First 2 chars of username
-        '9',                                -- Static '9'
-        SUBSTRING(od.oid, 2, 2),            -- Middle 2 digits of order ID
-        SUBSTRING(p.pid, 2, 2)              -- Middle 2 digits of payment ID
+        LOWER(SUBSTRING(u.username, 1, 2)), -- first two letters of username
+        LEFT(od.oid, 2),                    -- first two digits of oid
+        RIGHT(p.pid, 2),                    -- last two digits of pid
+        RIGHT(u.mobile_no, 2)               -- last two digits of the mobile_no
     ) as invoice_no,
     od.created_at
 FROM 
